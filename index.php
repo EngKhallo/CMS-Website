5d6121c5f0a5a80dc20fd0d3fcb170d3e2e5c9f4
EngKhallo <engkhalid.ict@gmail.com>
2022-04-12 14:14:13 +0300

    Fetch home content

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-﻿<!DOCTYPE html>
+﻿<?php
+include './Admin/config/connection.php';
+$conn = connect();
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -30,41 +35,56 @@
 
 <body class="home">
 
-<!-- Header Starts -->
-<?php include './includes/header.php'; ?>
-<!-- Header Ends -->
-<!-- Main Content Starts -->
-<section class="container-fluid main-container container-home p-0 revealator-slideup revealator-once revealator-delay1">
-    <div class="color-block d-none d-lg-block"></div>
-    <div class="row home-details-container align-items-center">
-        <div class="col-lg-4 bg position-fixed d-none d-lg-block"></div>
-        <div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
-            <div>
-                <img src="../dark/img/hakaale.jpg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="my picture" />
-                <h1 class="text-uppercase poppins-font">I'm Mukhtar Hakaale.<span>The CEO of SomDigital Solutions</span></h1>
-                <p class="open-sans-font">In today’s rapidly evolving business landscape, digital, data, UX, and technology solutions could not be more critical to an organization’s overall strategy, growth, and success.</p>
-                <a class="button" href="about.html">
+    <!-- Header Starts -->
+    <?php include './includes/header.php'; ?>
+    <!-- Header Ends -->
+    <!-- Main Content Starts -->
+    <section class="container-fluid main-container container-home p-0 revealator-slideup revealator-once revealator-delay1">
+        <div class="color-block d-none d-lg-block"></div>
+        <div class="row home-details-container align-items-center">
+            <div class="col-lg-4 position-fixed d-lg-block">
+                <?php
+                $sql = "SELECT Image FROM home WHERE id=1";
+                $result = mysqli_query($conn, $sql);
+
+                while ($row = mysqli_fetch_object($result)) : ?>
+                    <img src="<?php echo './Admin/Images' . $row->Image; ?>" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="No Image at all"/>
+                <?php endwhile; ?>
+            </div>
+            <div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
+                <div>
+                    <h1 class="text-uppercase poppins-font">I'm Mukhtar Hakaale.<span>The CEO of SomDigital Solutions</span></h1>
+                    <p class="open-sans-font">
+                        <?php
+                        $sql = "SELECT About FROM home WHERE id=1";
+                        $result = mysqli_query($conn, $sql);
+
+                        while ($row = mysqli_fetch_object($result)) : ?>
+                    <p><?php echo $row->About; ?></p>
+                <?php endwhile; ?>
+                </p>
+                <a class="button" href="about.php">
                     <span class="button-text">more about me</span>
                     <span class="button-icon fa fa-arrow-right"></span>
                 </a>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- Main Content Ends -->
+    </section>
+    <!-- Main Content Ends -->
 
-<!-- Template JS Files -->
-<script src="js/jquery-3.5.0.min.js"></script>
-<script src="js/preloader.min.js"></script>
-<script src="js/fm.revealator.jquery.min.js"></script>
-<script src="js/imagesloaded.pkgd.min.js"></script>
-<script src="js/masonry.pkgd.min.js"></script>
-<script src="js/classie.js"></script>
-<script src="js/cbpGridGallery.js"></script>
-<script src="js/jquery.hoverdir.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/custom.js"></script>
+    <!-- Template JS Files -->
+    <script src="js/jquery-3.5.0.min.js"></script>
+    <script src="js/preloader.min.js"></script>
+    <script src="js/fm.revealator.jquery.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpGridGallery.js"></script>
+    <script src="js/jquery.hoverdir.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/custom.js"></script>
 
 </body>
 
